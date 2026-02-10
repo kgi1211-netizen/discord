294b58f994c6025a2b253d63d097deed0fe293f1
kgi1211-netizen <kgi1211@gmail.com>
2026-02-10 22:14:33 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,15 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log("봇이 온라인 상태입니다");
+
+  try {
+    const channel = await client.channels.fetch("여기에_채널ID");
+    await channel.send("봇이 정상적으로 실행되었습니다");
+  } catch (err) {
+    console.error("메시지 전송 실패:", err);
+  }
 });
 
 client.login(process.env.TOKEN);
