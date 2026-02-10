ac74164ae45f69c0b41162ed9e01f4d3b73dce39
kgi1211-netizen <kgi1211@gmail.com>
2026-02-10 22:15:43 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.once("ready", async () => {
   console.log("봇이 온라인 상태입니다");
 
   try {
-    const channel = await client.channels.fetch("여기에_채널ID");
+    const channel = await client.channels.fetch("1464170323091001415");
     await channel.send("봇이 정상적으로 실행되었습니다");
   } catch (err) {
     console.error("메시지 전송 실패:", err);
@@ -16,3 +16,4 @@ client.once("ready", async () => {
 });
 
 client.login(process.env.TOKEN);
+
